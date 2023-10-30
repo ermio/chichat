cbae8402175b4bf8df9ee4f2593cae4e93b7b4e3
Ermia <36486470+ermio@users.noreply.github.com>
2023-10-30 15:01:56 +0300

    Update footer.tsx
    Added etherscan link as donor list

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,8 +14,8 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
     >
       Free chatGPT interface built with{' '}
       <ExternalLink href="https://github.com/vercel-labs/ai-chatbot">Vercel</ExternalLink> and sponsored by{' '}
-      <ExternalLink href="https://twitter.com/pachapapaa">
-        Pachapapa
+      <ExternalLink href="https://etherscan.io/address/0x0c4f3263ac508FE739010EDB8765dA50Ee271C38">
+        Donors
       </ExternalLink>
       .
     </p>

eb6e6683cb9077febaada0219bba875f16666901
Ermia <36486470+ermio@users.noreply.github.com>
2023-10-30 12:40:09 +0300

    Update layout.tsx
    Edited page title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ import { Header } from '@/components/header'
 
 export const metadata: Metadata = {
   title: {
-    default: 'Next.js AI Chatbot',
+    default: 'ChiChat | Personal AI Assistant',
     template: `%s - Next.js AI Chatbot`
   },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  description: 'A free AI-powered chatbot',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: 'white' },
     { media: '(prefers-color-scheme: dark)', color: 'black' }

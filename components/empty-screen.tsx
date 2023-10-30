b7e1120c597af4c9f9d4e3315a43deaae75bf5bf
Ermia <36486470+ermio@users.noreply.github.com>
2023-10-30 14:53:59 +0300

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -24,7 +24,7 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to ChiChat, your Personal AI Assistant!
+          Welcome to ChiChat, your personal AI assistant!
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
           This is a free chatGPT interface built with{' '}
